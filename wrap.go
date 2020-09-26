761ab740936535939e51eface9574271a5c7c4a8
Michael Muré <batolettre@gmail.com>
2020-09-26 19:09:49 +0200

    add support for OSC sequences

--- wrap.go
+++ wrap.go
@@ -389,16 +389,14 @@ func splitWord(word string, length int) (string, string) {
 	runes := []rune(word)
 	var result []rune
 	added := 0
-	escape := false
 
 	if length == 0 {
 		return "", word
 	}
 
+	var ed EscapeDetector
 	for _, r := range runes {
-		if r == '\x1b' {
-			escape = true
-		}
+		ed.Witness(r)
 
 		width := runewidth.RuneWidth(r)
 		if width+added > length {
@@ -408,15 +406,11 @@ func splitWord(word string, length int) (string, string) {
 
 		result = append(result, r)
 
-		if !escape {
+		if !ed.InEscape() {
 			added += width
 			if added >= length {
 				break
 			}
-		}
-
-		if r == 'm' {
-			escape = false
 		}
 	}
 
